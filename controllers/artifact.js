443edec8cb47e13d52755fd7aa23e1d1d5b5c319
Ruby King <ruby.sz.king@gmail.com>
2019-08-02 16:00:54 -0400

    redirect to home

---
+++ controllers/artifact.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Artifact = require('../models/artifact.js');
+
+
+// router.get('/index', (req, res) => {
+//     res.render('index.ejs');
+// });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/artifact';
+const session = require('express-session');
+const methodOverride = require('method-override');
 
 // MODELS
 const Artifact = require ('./models/artifact.js');
-
-// CONTROLLERS
-const artifactController = require('./controllers/artifact.js');
-app.use('/artifact', artifactController);
-const newController = require('./controllers/new.js');
-app.use('/new', newController);
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));
@@ -27,10 +23,30 @@ app.use(
 app.use(express.json());
 app.use(express.static('public'));
 
+// CONTROLLERS
+const artifactController = require('./controllers/artifact.js');
+app.use('/Artifact', artifactController);
+
+// const newController = require('./controllers/new.js');
+// app.use('/New', newController);
+
+// const editController = require('./controllers/edit.js');
+// app.use('/Edit', editController);
+
+
 // INDEX FOR HEROKU
 app.get('/', (req, res) => {
-    res.send('running');
+    res.redirect('/home');
 });
+
+app.get('/home', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.get('/index', (req, res) => {
+    res.render('index.ejs');
+})
+
 
 // MONGOOSE
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true}, () => {
