05625c482f096f927945f36d8acd536510044dad
Ruby King <ruby.sz.king@gmail.com>
2019-08-05 16:29:52 -0400

    images linked to show route

--- controllers/artifact.js
+++ controllers/artifact.js
@@ -2,21 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Artifact = require('../models/artifact.js');
 
-router.get('/:tag', (req, res) => {
+router.get('/tag/:tag', (req, res) => {
     const dataTag = req.params.tag;
+    const id = req.params.id;
     Artifact.find({style: dataTag}, (error, data) => {
         console.log('data: ',data)
         res.render('../views/artifact/first.ejs', {
             currentData: data,
-            tag: req.params.tag
-
+            tag: req.params.tag,
+            id: id
         })
-
     })
 });
 
-router.get('/:id', (req, res) => {
-    res.render('../views/artifact/show.ejs');
+router.get('/id/:id', (req, res) => {
+    const dataId = req.params.id;
+    Artifact.findById(dataId, (error, data) => {
+        if (error) {
+            console.log(error)
+        } else {
+            res.render('artifact/show.ejs', {
+                dataId: dataId,
+                data: data
+            })
+
+        }
+    })
 });
 
 router.get('/:id/edit', (req, res) => {
